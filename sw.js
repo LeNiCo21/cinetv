4237658608d61e48322a92110ae6805845b4fcbc
LeNiCo21 <canton.nicolas@orange.fr>
2026-06-20 23:26:43 +0200

    Replace PNG icons with JPG icons in cache

--- sw.js
+++ sw.js
@@ -16,9 +16,9 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './icon-maskable-512.png'
+  './icon-192.jpg',
+  './icon-512.jpg',
+  './icon-maskable-512.jpg'
 ];
 
 self.addEventListener('install', (event) => {
